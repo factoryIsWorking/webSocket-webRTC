24b588e1cf68cb90a3af9bcf11fe11e2cf9b18d9
LeeHanSeong <jkhk7788@gmail.com>
2021-09-10 01:12:28 +0900

    webSocket added

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,7 +1,34 @@
+import http from "http"
 import express from "express"
+import Websocket from "ws"
+
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+app.get("/*",(req,res)=>{
+    res.redirect("/");
+});
 
-app.listen(3000);
+const handleListen = () => console.log("http://localhost:3000");
+
+const server = http.createServer(app);
+const wss = new Websocket.Server({server});
+
+wss.on("connection",(socket)=>{
+    console.log("connected");
+    socket.on("close",()=>{
+        console.log("disconnected");
+    });
+    socket.on("message", (message)=>{
+        console.log(message);
+    });
+    socket.send("hello");
+});
+
+server.listen(3000,handleListen);
